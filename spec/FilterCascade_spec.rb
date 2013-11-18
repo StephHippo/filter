cfd557f2abb8712537e7de6c65c270bfe4e6b338
stephhippo <stephanie.hippo@gmail.com>
2013-11-18 00:12:06 -0500

    Added tests for ScalarLinearFilter and updated Cascade.

---
+++ spec/FilterCascade_spec.rb
@@ -0,0 +1,21 @@
+require './FilterCascade.rb'
+require './MaxFilter.rb'
+require './MinFilter.rb'
+
+describe 'FilterCascade' do
+  before(:each) do
+    @cascade = FilterCascade.new()
+    @max = MaxFilter.new(2)
+    @min = MinFilter.new(3)
+    @cascade.add_filter(@max)
+    @cascade.add_filter(@min)
+  end
+
+  it 'should chain link all inputs and outputs' do
+    @cascade.cascade(-1).should be -1
+    @cascade.cascade(3).should be -1
+    @cascade.cascade(1).should be -1
+    @cascade.cascade(2).should be 2
+    @cascade.cascade(1).should be 2
+  end
+end
--- spec/ScalarLinearFilter_spec.rb
+++ spec/ScalarLinearFilter_spec.rb
@@ -1,15 +1,16 @@
-#require './ScalarLinearFilter.rb'
-#
-#describe 'ScalarLinearFilter' do
-#  before(:each) do
-#    @sl = ScalarLinearFilter.new
-#  end
-#
-#  it 'should find the max of a series of inputs' do
-#    @sl
-#  end
-#
-#  it 'should assert that the inputs must be numbers' do
-#    lambda{@sl.get_mean(Object.new)}.should raise_error
-#  end
-#end
+require './ScalarLinearFilter.rb'
+
+describe 'ScalarLinearFilter' do
+  before(:each) do
+    @sl = ScalarLinearFilter.new([0.5,0.5], [0.1])
+  end
+
+  it 'should find the max of a series of inputs' do
+    @sl.get_output(-1).should be -0.5
+    @sl.get_output(1).should be 0.05
+  end
+
+  #it 'should assert that the inputs must be numbers' do
+  #  lambda{@sl.get_output(Object.new)}.should raise_error
+  #end
+end

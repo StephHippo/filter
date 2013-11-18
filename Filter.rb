e6289f70a83a1c9c544cc59cb49fff869168f91f
stephhippo <stephanie.hippo@gmail.com>
2013-11-17 20:26:26 -0500

    Removed unnecessary code.

--- Filter.rb
+++ Filter.rb
@@ -2,6 +2,7 @@ class Filter
 
   #repeatedly takes an input value
   #produces an output value
+  attr_reader :n
 
   def initialize
     @inputs = []
@@ -11,23 +12,9 @@ class Filter
   #TODO: Use an options hash?
   def reset(reset_in_val = 0, reset_out_val = 0)
     #print inputs and outputs to be discarded
-    to_s
+    to_s unless @inputs.is_empty?
     @inputs.map!{|input| input = reset_in_val}
     @outputs.map!{|output| output = reset_out_val}
-  end
-
-  #TODO: Opportunity to refactor last 3 lines
-  #TODO: Best way to handle?
-  #takes an input, gives an output
-  def function(input, n = @inputs.length)
-    @inputs << input
-    if block_given?
-      output = yield input
-    else
-      output = input
-    end
-    @outputs << output
-    output
   end
 
   def input_value(input)
@@ -36,7 +23,6 @@ class Filter
 
   def to_s
     puts "Inputs  \t  Outputs"
-    #TODO: If we decide to support archived inputs, add functionality here
     #for each input, put the output
     @inputs.each_with_index do |input, i|
       puts "#{input} \t #{@outputs[i]}"
